9b8266cd83dcc9cec900cd34e6f4f13afc3c843b
Nilox42 <justicemauerman@gmail.com>
2020-11-10 11:48:36 +0100

    Added Controlelr Class

---
+++ Core/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nilox2DGameEngine.Core
+{
+    class Controller
+    {
+    }
+}
