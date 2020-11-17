82145616ccbf8746b781eba03c4f35741bda2410
Nilox42 <justicemauerman@gmail.com>
2020-11-17 16:39:47 +0100

    Added Enemy Class

---
+++ Character/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nilox2DGameEngine.Character
+{
+    public class Enemy
+    {
+
+
+
+    }
+}
